fae2bfc8b0ad80ad2e6f8e065372265df7299e4a
Raizooo78 <badrbouderga788@gmail.com>
2026-09-08 14:54:55 +0200

    test

--- prog1.py
+++ prog1.py
@@ -3,5 +3,6 @@ import fonction as DGFIP
 a = int(input("nombre  : "))
 b = int(input("puissance  : "))
 
-boum = DGFIP.puiss(a, b)
+# boum = DGFIP.puiss(2.5, 3)
+boum = DGFIP.puiss(2.5, 3)
 print(boum)
