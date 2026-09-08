7e28cba07e36c305ab32cb5ab2043e67f603bd4c
Raizooo78 <badrbouderga788@gmail.com>
2026-09-08 17:49:58 +0200

    4.6

--- TP1/fonction.py
+++ TP1/fonction.py
@@ -1,13 +1,17 @@
 def puiss(a, b):
     if not type(a) is int or not type(b) is int:
         raise TypeError("hell nah")
+    if a == 0 and b < 0:
+        raise ValueError("pas poddible")
+    if a == 0:
+        return 0
     result = 1
-    if b<0:
-       for i in range(-b):
-          result *= a 
-       if a == 0 :
-          raise ValueError("pas poddible")		
-       else : return (1/result)
+    if b < 0:
+        for i in range(-b):
+            result *= a
+        return 1 / result
     for i in range(b):
-       result *= a
-    return (result)
+        result *= a
+    return result
+
+puissance = puiss
--- TP1/prog1.py
+++ TP1/prog1.py
@@ -3,6 +3,5 @@ import fonction as DGFIP
 a = int(input("nombre  : "))
 b = int(input("puissance  : "))
 
-# boum = DGFIP.puiss(2.5, 3)
-boum = DGFIP.puiss(2.5, 3)
+boum = DGFIP.puiss(a, b)
 print(boum)
