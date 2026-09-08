fb4d5f84f4ebaa63f7205aabead26eb649d4642f
Raizooo78 <badrbouderga788@gmail.com>
2026-09-08 17:24:49 +0200

    4.6

--- TP1/fonction.py
+++ TP1/fonction.py
@@ -1,4 +1,13 @@
 def puiss(a, b):
     if not type(a) is int or not type(b) is int:
         raise TypeError("hell nah")
-    return a ** b
+    result = 1
+    if b<0:
+       for i in range(-b):
+          result *= a 
+       if a == 0 :
+          raise ValueError("pas poddible")		
+       else : return (1/result)
+    for i in range(b):
+       result *= a
+    return (result)
